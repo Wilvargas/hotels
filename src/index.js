27cb6e9ea8d890344f3664374ba46154330bf428
“wilvargas” <wilvargas7gmail.com>
2023-08-01 15:43:44 -0500

    Acomodación de Variables, y funciones. Creación de la Function getFlagImageURL para establecer la ruta de las banderas que diseñé para el proyecto ubicadas localmente. Creación de la función getPriceText () para estableces el cambio de número en el precio por la repetición de $ de acuerdo al valor. Acomodación standar de los estilos para la disposición de 1024px (Desktop)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ function getFlagImageUrl(country) {
   return flagPath;
 }
 
+// Función para obtener el texto del precio con los signos de dólar adecuados
+function getPriceText(price) {
+  return "$".repeat(price);
+}
+
 function showHoteles() {
   const containerElement = document.querySelector(".container--cards");
 
@@ -16,42 +21,53 @@ function showHoteles() {
       data.forEach((hotel) => {
         // Creamos un elemento <article> para representar cada hotel
         const articleElement = document.createElement("article");
-        articleElement.classList.add("hotel"); // Opcional: agregar una clase CSS "hotel" para estilos
+        articleElement.classList.add("hotel"); 
 
         // Creamos y agregamos los elementos para mostrar la información del hotel
         const nameElement = document.createElement("h2");
         nameElement.textContent = hotel.name;
         articleElement.appendChild(nameElement);
+        
+        const flagElement = document.createElement("img");
+        flagElement.src = getFlagImageUrl(hotel.country);
+        flagElement.alt = `Flag of ${hotel.country}`;
+        flagElement.classList.add("img-flag");
+        articleElement.appendChild(flagElement);
 
         const countryElement = document.createElement("p");
-        countryElement.textContent = `Country: ${hotel.country}`;
+        countryElement.textContent = `${hotel.country}`;
+        countryElement.classList.add("p-c");
         articleElement.appendChild(countryElement);
 
+        const roomsElement = document.createElement("p");
+        roomsElement.textContent = `${hotel.rooms} rooms`;
+        roomsElement.classList.add("p-r");
+        articleElement.appendChild(roomsElement);
+
         const priceElement = document.createElement("p");
-        priceElement.textContent = `Price: ${hotel.price}`;
+        priceElement.textContent = getPriceText(hotel.price); // Obtenemos el texto del precio con los signos de dólar
+        priceElement.classList.add("p-p");
         articleElement.appendChild(priceElement);
+
+        const bookButton = document.createElement("button");
+        bookButton.textContent = "Book it!";
+        articleElement.appendChild(bookButton);
 
         const sizeElement = document.createElement("p");
         sizeElement.textContent = `Size: ${hotel.size}`;
+        sizeElement.classList.add("p-s");
         articleElement.appendChild(sizeElement);
 
         const photoElement = document.createElement("img");
         photoElement.src = hotel.photo;
         photoElement.alt = hotel.name;
+        photoElement.classList.add("img-hotel");
         articleElement.appendChild(photoElement);
 
         const descriptionElement = document.createElement("p");
         descriptionElement.textContent = `Description: ${hotel.description}`;
+        descriptionElement.classList.add("p-d");
         articleElement.appendChild(descriptionElement);
-
-        const roomsElement = document.createElement("p");
-        roomsElement.textContent = `Rooms: ${hotel.rooms}`;
-        articleElement.appendChild(roomsElement);
-
-        const flagElement = document.createElement("img");
-        flagElement.src = getFlagImageUrl(hotel.country);
-        flagElement.alt = `Flag of ${hotel.country}`;
-        articleElement.appendChild(flagElement);
 
         // Agregamos el elemento <article> al contenedor (<div class="container--cards">)
         containerElement.appendChild(articleElement);
