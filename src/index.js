df2416554ce0a037774b0e278538b85018b6def1
Wilfredo Vargas Vinasco <134229656+Wilvargas@users.noreply.github.com>
2023-08-14 18:00:13 -0500

    Hotels (#3)
    * Borrando folder src y archivos html y .js
    
    * first commit
    
    * Añadiendo archivos que borré anteriormente, como videos e imagenes. también logré la integración con la API y la colocación de las cards en la pag
    
    * Acomodación de Variables, y funciones. Creación de la Function getFlagImageURL para establecer la ruta de las banderas que diseñé para el proyecto ubicadas localmente. Creación de la función getPriceText () para estableces el cambio de número en el precio por la repetición de $ de acuerdo al valor. Acomodación standar de los estilos para la disposición de 1024px (Desktop)
    
    * Borrando comentarios innecesarios
    
    * Ajuste del Header. Introducción de iconos a los selectores e inputs, y posicionamiento de textos dentro de los mismos
    
    * Intenté no perder la funcionalidad de los inputs type date, al añadir un icono diferente y poner oculto el icono de calendario que viene por default pero no lo logré. No se aún como poder hacer click sobre todo ese input, en cualquier región del mismo y que me muestre el calendario. Sigo buscando alternativas para ello. Por lo pronto dejo los dos iconos
    
    * Avance en la logica de los filtros. Funciona el selector all countries y el button clear, pero aun no logro filtrar por paises, precios o tamaños. Sigo en ese proposito
    
    * Ajuste responsive con grid para las cards, sin necesidad de implementar media queries
    
    * Actualización de proyecto, trabajando en la lógica de los filtros. Se añadió info adicional relacionada con los sizes.
    
    * Ajustes en las clases de los rows 86, 87, y 88 que me llevaron a desbloquear el código. La lógica estaba correcta, pero tenía fallos en la llamada de las clases. También se añadió la función 'hotel sizes' que me permitió asignar tamaños a los hoteles. El código ya funciona en un 95%. Los filtros de País, Precio, y Tamaño funcionan perfectamente. Tambien el botón 'clear'.Falta el filtrado por fechas de ida y vuelta y los mensajes de alerta cuando no se logra la busqueda solicitada
    
    * Recuperación del código. Se había roto hasta la madre jajaja. Establecimiento de fechas minimas en los atributos de los inputs type='date'. El input1 tiene como parametro la fecha actual y quita la posibilidad de escoger una fecha del pasado. Para esto fue necesario hacer conversiones con los tipos de fechas. Y el input2 tiene como parametro la fecha que el usuario ha seleccionado y que el listener a capturado. A este punto no está funcionando el filtrado de los inputs, pero espero mañana dejarlos listos
    
    * Check Daniel Calle - Filtro de fechas
    
    * Filtrado finalizado. Bendito sea por siempre el Dios de Israel
    
    * Estaba teniendo problemas con el botón clear y decidí llamar la variable SelectedDays con valor de 0, y posteriormente dentreo de la función clearFilters, reinicializar selectedDays para que me mostrará todos los hoteles despues de aplicar diferentes combinaciones con todos los filtros en ejecución
    
    * Cración de mensaje NotFoundHotels
    
    ---------
    
    Co-authored-by: “wilvargas” <“wilvargas7ågmail.com“>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { hotelsData } from "./hotels.js";
-
 /* FLAGS */
 function getFlagImageUrl(country) {
   const flagPath = `./assets/${country}-flag.png`;
@@ -9,14 +8,30 @@ function getFlagImageUrl(country) {
 function getPriceText(price) {
   return "$".repeat(price);
 }
+// FECHA
+let currentDate = new Date();
+
+let year = currentDate.getFullYear();
+let month = (currentDate.getMonth()+1).toString().padStart(2, '0');
+let day = currentDate.getDate().toString().padStart(2, '0');
+
+let minCurrentDate = `${year}-${month}-${day}`;
+
+let selectedDays = 0;
 /* FUNCTION - SHOW HOTELS  */
+
 function showHotels() {
   const containerElement = document.querySelector(".container--cards");
   const allCountriesClick = document.getElementById("filter-countries");
   const allPricesClick = document.getElementById("filter-prices");
   const allSizesClick = document.getElementById("filter-sizes");
   const clearButton = document.getElementById("clearButton")
-  
+  let inputDate1 = document.getElementById("fecha1");
+  inputDate1.setAttribute("min", minCurrentDate);
+  console.log(inputDate1);
+  const inputDate2 = document.getElementById("fecha2");
+  //La asignación del valor al input2 lo hice despues del evento en el campo fecha ocurrido en el input1
+
   const hotelSizes = (rooms) => {
     if (rooms <= 10) {
       return 1
@@ -27,19 +42,18 @@ function showHotels() {
     if (rooms >20) {
       return 3
     }
-  } 
-
+  }
   /* LLAMADA A LA API PARA OBTENER LA INFO DE LOS HOTELES */
   hotelsData()
-    .then((response) => response.json())
-    .then((data) => {
-      /* ITERAR SOBRE LA DATA Y CREAR ELEMENTOS */
-      data.forEach((hotel) => {
-        const articleElement = document.createElement("article");
-        articleElement.classList.add("hotel"); 
-        /* TITLE H2 - CARDS */
-        const nameElement = document.createElement("h2");
-        nameElement.textContent = hotel.name;
+  .then((response) => response.json())
+  .then((data) => {
+    /* ITERAR SOBRE LA DATA Y CREAR ELEMENTOS */
+    data.forEach((hotel) => {
+      const articleElement = document.createElement("article");
+      articleElement.classList.add("hotel");
+      /* TITLE H2 - CARDS */
+      const nameElement = document.createElement("h2");
+      nameElement.textContent = hotel.name;
         articleElement.appendChild(nameElement);
         /* IMG FLAGS */
         const flagElement = document.createElement("img");
@@ -59,7 +73,7 @@ function showHotels() {
         articleElement.appendChild(roomsElement);
         /* PRICE PARAGRAPH */
         const priceElement = document.createElement("p");
-        priceElement.textContent = getPriceText(hotel.price); 
+        priceElement.textContent = getPriceText(hotel.price);
         priceElement.classList.add("p-p");
         articleElement.appendChild(priceElement);
         /* BUTTON - BOOK IT! */
@@ -87,53 +101,115 @@ function showHotels() {
         articleElement.classList.add(`price-${hotel.price}`);
         articleElement.classList.add(`size-${hotelSizes(hotel.rooms)}`);
         containerElement.appendChild(articleElement);
+        /* AVAILABILITY FROM Y TO */
+        articleElement.setAttribute("availabilityFrom", hotel.availabilityFrom);
+        articleElement.setAttribute("availabilityTo", hotel.availabilityTo);
+        containerElement.appendChild(articleElement);
+        const availabilityFrom = hotel.availabilityFrom;
+        const availabilityTo = hotel.availabilityTo;
+        const availableDays = (availabilityTo - availabilityFrom) / (24 * 60 * 60 * 1000);
+        // CREO EL ATRIBUTO availableDays PARA ACCEDER FACILMENTE AL # DE DÍAS DISPONIBLES EN LOS HOTELES
+        articleElement.setAttribute("availableDays", availableDays);
+        console.log(`${hotel.name}: ${availableDays} días disponibles`);
+        //MESSAGE HOTELS NOT FOUND 
+        const notFoundSection = document.createElement("div");
+        notFoundSection.classList.add("hotelsNotFound");
+        notFoundSection.style.display = "none"; //Quiero ocultar el mensaje desde el inicio.
+
+        const infoIcon = document.createElement("img");
+        infoIcon.classList.add("img-info")
+        infoIcon.src = "./assets/info-icon.png";
+        infoIcon.alt = "Info Icon";
+
+        const notfoundText = document.createElement("p");
+        notfoundText.textContent = "No hotel left with those filters. Try a new combination of them";
+        notfoundText.classList.add("p-notFoundHotel")
+        
+        notFoundSection.appendChild(infoIcon);
+        notFoundSection.appendChild(notfoundText);
+        containerElement.appendChild(notFoundSection);
       });
-      
       /* CREACIÓN DE EVENTOS SOBRE FILTROS - FILTRAR HOTELES Y LIMPIAR BOTÓN */
       /* la función filterHotels se ejecutará cada vez que el Elemento contenido en allCountriesClick cambie */
       allCountriesClick.addEventListener("change", () => filterHotels());
       allPricesClick.addEventListener("change", () => filterHotels());
       allSizesClick.addEventListener("change", () => filterHotels());
       clearButton.addEventListener("click", () => clearFilters());
-
+      
+      inputDate1.addEventListener("change", () => {
+        const startDate = new Date(inputDate1.value).getTime();
+        console.log(startDate);  
+        const minDate = new Date(startDate);
+        minDate.setDate(minDate.getDate() + 1);
+        const minDateFormatted = `${minDate.getFullYear()}-${(minDate.getMonth() + 1).toString().padStart(2, '0')}-${minDate.getDate().toString().padStart(2, '0')}`;
+        
+        // OTRA FORMA DE ASIGNAR UN VALOR AL ATRIBUTO "MIN"
+        inputDate2.min = minDateFormatted;
+        console.log(inputDate2.value)
+          filterHotels(); // Llamar a la función filterHotels después de validar la fecha
+      });
+      inputDate2.addEventListener('change', () => {
+        const startDate = new Date(inputDate1.value).getTime();
+        const endDate= new Date(inputDate2.value).getTime()
+        console.log(endDate)
+        selectedDays = (endDate - startDate) / (24 * 60 * 60 * 1000); // Calcula los días seleccionados
+        
+        filterHotels(); // Llamar a la función filterHotels después de validar la fecha
+      });
       filterHotels();
     })
     .catch((error) => {
       console.error("Error fetching hotels data:", error);
     });
-}
+  }
+    
 /* FILTER HOTELS */
 function filterHotels() {
   const selectedCountry = document.getElementById("filter-countries").value;
   const selectedPrice = document.getElementById("filter-prices").value;
   const selectedSize = document.getElementById("filter-sizes").value;
-  const hotelCards = document.querySelectorAll(".hotel");
-
-  console.log(hotelCards[0].classList);
-
-
+  const hotelCards = document.querySelectorAll(".hotel");   
+  let noHotelsFound = true 
   /* VERIFICO SI UNA CARD DEBE MOSTRARSE O NO */
   hotelCards.forEach(card => {
     const countryMatch = selectedCountry === "all" ? true : card.classList[1].includes(selectedCountry);
     const priceMatch = selectedPrice === "all" ? true : card.classList[2].includes(selectedPrice);
     const sizeMatch = selectedSize === "all" ? true : card.classList[3].includes(selectedSize);
-
     console.log(selectedCountry,selectedPrice,selectedSize);
-
-    if (countryMatch && priceMatch && sizeMatch) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    };
+    
+    const availableDays = parseFloat(card.getAttribute("availableDays"));
+    const dateMatch = selectedDays > availableDays ? false : true;
+    
+    console.log(selectedDays);
+    console.log(availableDays)
+    if (countryMatch && priceMatch && sizeMatch && dateMatch) {
+        card.style.display = "block";
+        noHotelsFound = false; // Si se encontró al menos un hotel
+      } else {
+        card.style.display = "none";
+      }
   });
+  //MESSAGE CONDITIONAL
+      const notFoundSection = document.querySelector(".hotelsNotFound");
+    if (noHotelsFound) {
+      notFoundSection.style.display = "block";
+    } else {
+      notFoundSection.style.display = "none";
+    }
 };
 
-
 function clearFilters() {
+  console.log("clearFilters() function called");
   document.getElementById("filter-countries").value = "all";
   document.getElementById("filter-prices").value = "all";
   document.getElementById("filter-sizes").value = "all";
-  
+
+  console.log("Clearing date fields");
+  document.getElementById("fecha1").value = "";
+  document.getElementById("fecha2").value = "";
+
+  selectedDays = 0; // Reinicio de variable - Estaba teniendo problemas con el btn clear.
   filterHotels();
 }
 window.onload = showHotels;
+
